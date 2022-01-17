e2e2427b1e106b96f4b3b417b7956cec40640a4f
Adam <94177926+okdemsky@users.noreply.github.com>
2022-01-17 15:25:56 -0500

    Change new years to grad date

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById('hours')
 const minsEl = document.getElementById('mins')
 const secondsEl = document.getElementById('seconds')
 
-const newYears = '1 jan 2022';
+const newYears = '14 Apr 2022';
 
 function countdown() {
     const newYearsDate = new Date(newYears)
